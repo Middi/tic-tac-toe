04801e1f587a66b850312b91393b4f774dbe3c54
“Richard Middleton” <Richard@richardmiddleton.me>
2017-05-17 01:29:08 +0200

    event listeners and object with turns

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 var turn = "x";
+var spaces = {
+    "1": 0,
+    "2": 0,
+    "3": 0,
+    "4": 0,
+    "5": 0,
+    "6": 0,
+    "7": 0,
+    "8": 0,
+    "9": 0
+};
 
-if(turn === "x") {
-    turn = "y";
+var box = document.getElementsByClassName('square');
+
+for(var i = 0; i < box.length; i++) {
+box[i].addEventListener('click', function () {
+
+    if (turn === "x") {
+        this.innerHTML = "O";
+        spaces[this.id] = 1;
+        turn = "o";
+        console.log(turn);
+        console.log(spaces);
+    }
+    else {
+        this.innerHTML = "X";
+        spaces[this.id] = 2;
+        turn = "x";
+        console.log(turn);
+        console.log(spaces);
+    }
+console.log('yeah');
+});
 }
-else {
-    turn = "x";
-}
+
+
+
+// var plus = document.getElementById('plus');
+
+// plus.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     timeLeft = timeLeft + 60;
+//     counterDom.innerHTML = (convertMins(timeLeft - counter));
+
+// });
